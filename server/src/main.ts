6b6d1111d32b5c80955a4524ce918f155ce23d6b
abijoserah <serahabijo94@gmail.com>
2025-12-07 15:56:11 +0100

    Clean up Wild series 02

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,23 +1,14 @@
-// Load environment variables from .env file
 import "dotenv/config";
-
-// Check database connection
-// Note: This is optional and can be removed if the database connection
-// is not required when starting the application
 import "../database/checkConnection";
-
-// Import the Express application from ./app
+import type { RequestHandler } from "express";
 import app from "./app";
 
-import type { RequestHandler } from "express";
-
-// Get the port from the environment variables
 const port = process.env.APP_PORT;
 
 const sayWelcome: RequestHandler = (req, res) => {
   res.send("Welcome to wild series !");
 };
-// Start the server and listen on the specified port
+
 app
   .listen(port, () => {
     console.info(`Server is listening on port ${port}`);
